00b7a18f871df979deaa5942c1736bc6b6ac1ef0
Gerardo Alderete Flores <ge_call@hotmail.com>
2025-02-27 08:40:10 -0600

    Se actualiza ruta de inicio

--- src/App.tsx
+++ src/App.tsx
@@ -101,7 +101,7 @@ function App() {
               </ProtectedRoute>
             }
           />
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Login />} />
         </Routes>
       </main>
       </HashRouter>
